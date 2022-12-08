f0cdd371855f02b9d56fe303eb04cafa8e2bea5b
Mesfin-1 <inbyflame@gmail.com>
2022-12-08 06:12:39 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,10 +4,10 @@
  *Return: Always 0 (Success)
  **/
 int main(void)
-{printf("size of a char: %d bytes(s)\n", sizeof(char));
-printf("size of an int: %d bytse(s)\n", sizeof(int));
-printf("size of a long int: %d bytes(s)", sizeof(long int));
-printf("size of a long long int: %d bytes(s)\n", sizeof(long long int));
-printf("size of a float: %d bytes(s)\n", sizeof(float));
+{printf("size of a char: %d bytes(s)\n",sizeof(char));
+printf("size of an int: %d bytse(s)\n",sizeof(int));
+printf("size of a long int: %d bytes(s)\n",sizeof(long int));
+printf("size of a long long int: %d bytes(s)\n",sizeof(long long int));
+printf("size of a float: %d bytes(s)\n",sizeof(float));
 return (0);
 }
