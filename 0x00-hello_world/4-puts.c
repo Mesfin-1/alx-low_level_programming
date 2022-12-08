b6cd9d4b30b05b7d72c0db3da4476c80646982d9
Mesfin-1 <inbyflame@gmail.com>
2022-12-08 05:42:35 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- *  main entry point
+ *  main : entry point
  *  return : 0 after printing function
  **/
-int main (void)
+int main(void)
 {
 puts("\"Programing is like building a multilingual puzzle");
 return (0);
