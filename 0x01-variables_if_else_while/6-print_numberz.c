2a41d83e6645e4b4df5fa7c3d2945052f4443844
Mesfin-1 <inbyflame@gmail.com>
2022-12-09 04:40:47 -0800

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,9 +7,10 @@
 int main(void)
 {
 int base_10;
-for (base_10 = 0; base_10 <= 10; base_10++)
+for (base_10 = 0; base_10 < 10; base_10++)
 {
-putchar(base_10);
+putchar((base_10 % 10) + '0');
+putchar('\n');
 }
 return (0);
 }
