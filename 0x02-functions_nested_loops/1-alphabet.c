59a9ce4171fd19567ff46220feac4e51f6aaa5f6
Mesfin-1 <inbyflame@gmail.com>
2022-12-13 09:19:43 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - Prints the alphabetic
+ * print_alphabet - function that will print the alphabet in lowercase
+ * Return: Always (Success)
+ **/
+void print_alphabet(void)
+{
+char h;
+for (h = 'a'; h <= 'z'; ++h)
+_putchar(h);
+_putchar('\n');
+}
