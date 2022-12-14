91b4022908ad1a4cdf24cb6fdae520e2ee66a7f5
Mesfin-1 <inbyflame@gmail.com>
2022-12-13 22:29:36 -0800

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Description: Print _isalpha
+ *Return: Always 0 (Success)
+ */
+int _isalpha(int c)
+{if ((c > 'a' && c < 'z') || (c > 'A' && c < 'Z'))
+{return (01);}
+else 
+{return (0);}}
