a2b7c055024fc25d0c7c9b27e5bf05d0cda9f1db
Mesfin-1 <inbyflame@gmail.com>
2022-12-19 04:07:42 -0800

    1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * main - check the code
- * swap_int  - swap the values of two integer 1 and b
+ * swap_int: swap the values of two integer 1 and b
  * @a: a pointer to an int that will be updated
  * @b: b pointer to an int that will be updated
  * Return: Always 0.
