78f948357e4d13a74ec586c63d2bca54b6ee24e1
Mesfin-1 <inbyflame@gmail.com>
2022-12-19 03:25:25 -0800

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,7 @@
 #include "main.h"
 
 /**
+ * main -  check the code
  * reset_to_98: - updates a integer value to 98
  * @n: a pointer to an int that will be updated
  * return: void that means is correct
