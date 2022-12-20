b071442293366387588a62a5b721dc207e424965
Mesfin-1 <inbyflame@gmail.com>
2022-12-19 21:18:46 -0800

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,7 +1,5 @@
 #include "main.h"
 
-
-
 /**
  * print_rev - imprime en reversa
  * @s: string
@@ -9,10 +7,17 @@
  */
 void print_rev(char *s)
 {
-int lon = 0, index;
-while (s[index++])
-lon++;
-for (index = lon - 1; index >= 0; index--)
-_putchar(s[index]);
-_putchar('\n');
+int longi = 0;
+int o;
+while (*s != '\0')
+{
+longi++;
+s++;
+}
+s--;
+for (o = longi; o > 0; o--)
+{
+_putchar(*s);
+s--;
+}_putchat('\n');
 }
