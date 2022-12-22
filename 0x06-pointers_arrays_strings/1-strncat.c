ebd49223e112da2a59de9cd0838d91afc05018a3
Mesfin-1 <inbyflame@gmail.com>
2022-12-22 01:19:39 -0800

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * _strncat - Concatenates two strings
  * @dest: The destination value
@@ -9,19 +10,10 @@
 char *_strncat(char *dest, char *src, int n)
 
 {
-int i, j;
-i = 0;
-while (dest[i] != '\0')
-{
-i++;
-}
-j = o;
-while (j < n && src[j] !='\0')
-{
-dest[dlen] = src[j];
-i++;
-j++;
-}
-dest[i] = '\0';
+int index = 0, dest_len = 0;
+while (dest[index++])
+dest_len++;
+for (index = 0; src[index] && index < n; index++)
+dest[dest_len++] = src[index];
 return (dest);
 }
