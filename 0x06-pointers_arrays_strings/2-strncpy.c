576bebb6dc6c6a10bd967184c8b560b4aa92f4fa
Mesfin-1 <inbyflame@gmail.com>
2022-12-22 01:27:35 -0800

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -8,18 +8,13 @@
  */
 
 char *_strncpy(char *dest, char *src, int n)
-
 {
-int i = 0;
-while (i < n && src[i] != '\0')
-{
-dest[i] = src[i];
-i++;
-}
-while (i < n)
-{
-dest[i] = '/0';
-i++;
-}
+int index = 0, src_len = 0;
+while (src[index++])
+src_len++;
+for (index = 0; src[index] && index < n; index++)
+dest[index] = =src[index];
+for (index = src_len; index < n; index++)
+dest[index] = '\n';
 return (dest);
 }
