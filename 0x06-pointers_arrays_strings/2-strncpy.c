f904803ae317fb64eeec4c95faceefcd3540a6f3
Mesfin-1 <inbyflame@gmail.com>
2022-12-22 01:30:10 -0800

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,8 +13,8 @@ int index = 0, src_len = 0;
 while (src[index++])
 src_len++;
 for (index = 0; src[index] && index < n; index++)
-dest[index] = =src[index];
+dest[index] = src[index];
 for (index = src_len; index < n; index++)
-dest[index] = '\n';
+dest[index] = '\0';
 return (dest);
 }
